6d61144ea5dbbe9ed7c7cdd745f9a48928d25a82
Safiyya A <146670910+lanternofsea@users.noreply.github.com>
2023-10-27 18:07:30 -0400

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,4 @@
+# Weekly log of pet patrons at a pet spa
+pet_spa_log = ['Candy', 'Sandy', 'Lemon', 'Ginger', 'Candy', 'Lemon', 'Candy']
+
+# Your code below this line
---
+++ sets_complete.py
@@ -0,0 +1,8 @@
+# Weekly log of pet patrons at a pet spa
+pet_spa_log = ['Candy', 'Sandy', 'Lemon', 'Ginger', 'Candy', 'Lemon', 'Candy']
+
+# Your code below this line
+visitor = set(pet_spa_log)
+
+for name in visitor:
+    print(name)
